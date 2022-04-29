813038a02704dd0b80b64d3fe10b4beabd4b083b
juanpacervin <juanpacervin@gmail.com>
2022-04-29 12:24:57 -0300

    Se agrega el proyecto

---
+++ reporteremito/_manifest_.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "reporteremito",
+
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+        Reporte para remitos preimpresos
+    """,
+
+    'author': "Waikiservicios",
+    'website': "",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Stock',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['stock.picking'],
+
+    # always loaded
+    'data': [
+        'report/report_remito.xml',
+        'report/report_remito_view.xml',
+    ]
+}
